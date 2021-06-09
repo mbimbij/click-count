6858b65923d43311d7fa0370c3bec0c83a62eb70
mbimbij <joseph.mbimbi@gmail.com>
2021-06-09 11:59:19 +0200

    Fix an error in the make-kubernetes.mk file that would force other target to define a variable "ENVIRONMENT"

--- infra/make-kubernetes.mk
+++ infra/make-kubernetes.mk
@@ -1,16 +1,13 @@
 ifndef APPLICATION_NAME
 $(error APPLICATION_NAME is not set)
 endif
-ifndef ENVIRONMENT
-$(error ENVIRONMENT is not set)
-endif
-ifndef AWS_REGION
-$(error AWS_REGION is not set)
-endif
 
 kubernetes-environment:
+	@if [ -z $(ENVIRONMENT) ]; then exit 255; fi
 	envsubst < kubernetes/cluster/cluster-template.yml > kubernetes/cluster/$(ENVIRONMENT)-cluster-processed.yml
 	eksctl create cluster -f  kubernetes/cluster/$(ENVIRONMENT)-cluster-processed.yml
 
 delete-kubernetes-environment:
 	eksctl delete cluster $(APPLICATION_NAME)-$(ENVIRONMENT)
+
+kubernetes-pipeline: s3-bucket

e39a3ad58dcca8831ebf5007dcb953ebab2cba72
mbimbij <joseph.mbimbi@gmail.com>
2021-06-09 16:32:26 +0200

    Add a target for an elasticache cluster

--- infra/make-kubernetes.mk
+++ infra/make-kubernetes.mk
@@ -3,15 +3,28 @@ $(error APPLICATION_NAME is not set)
 endif
 
 KUBE_PIPELINE_STACK_NAME=$(APPLICATION_NAME)-kube-pipeline
+ELASTICACHE_STACK_BASE_NAME=$(APPLICATION_NAME)-elasticache
 
-kubernetes-environment: requires-environment-set
-	echo "hello"
+kubernetes-cluster: requires-environment-set
 	envsubst < kubernetes/cluster/cluster-template.yml > kubernetes/cluster/$(ENVIRONMENT)-cluster-processed.yml
-	#eksctl create cluster -f  kubernetes/cluster/$(ENVIRONMENT)-cluster-processed.yml
+	eksctl create cluster -f  kubernetes/cluster/$(ENVIRONMENT)-cluster-processed.yml
 
-delete-kubernetes-environment: requires-environment-set
+delete-kubernetes-cluster: requires-environment-set
 	eksctl delete cluster $(APPLICATION_NAME)-$(ENVIRONMENT)
 
+elasticache: requires-environment-set
+	$(eval VPC_ID := $(shell aws cloudformation list-exports --region $(AWS_REGION) --query "Exports[?Name=='eksctl-$(APPLICATION_NAME)-$(ENVIRONMENT)-cluster::VPC'].Value" --output text))
+	$(eval PRIVATE_SUBNET_IDS := $(shell aws cloudformation list-exports --region $(AWS_REGION) --query "Exports[?Name=='eksctl-$(APPLICATION_NAME)-$(ENVIRONMENT)-cluster::SubnetsPrivate'].Value" --output text))
+	$(eval APPLICATION_SECURITY_GROUP_ID := $(shell aws cloudformation list-exports --region $(AWS_REGION) --query "Exports[?Name=='eksctl-$(APPLICATION_NAME)-$(ENVIRONMENT)-cluster::ClusterSecurityGroupId'].Value" --output text))
+	aws cloudformation deploy    \
+		--stack-name $(ELASTICACHE_STACK_BASE_NAME)-$(ENVIRONMENT)   \
+		--template-file environment/elasticache/redis-cluster.yml    \
+		--parameter-overrides     \
+		ApplicationName=$(APPLICATION_NAME)   \
+		Environment=$(ENVIRONMENT) \
+		VpcId=$(VPC_ID) \
+		SubnetIds=$(PRIVATE_SUBNET_IDS) \
+		ApplicationSecurityGroupId=$(APPLICATION_SECURITY_GROUP_ID)
 
 kubernetes-pipeline: s3-bucket
 	aws cloudformation deploy    \

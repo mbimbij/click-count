9c86fbaa0aaab5ec28e1288b9277c4de0c8de6a6
mbimbij <joseph.mbimbi@gmail.com>
2021-06-09 12:00:03 +0200

    - Initiate a pipeline with and ECR repository - Create a Makefile specific for the S3 bucket

---
+++ infra/make-bucket.mk
@@ -0,0 +1,8 @@
+s3-bucket:
+	aws cloudformation deploy    \
+          --stack-name $(S3_BUCKET_STACK_NAME)   \
+          --template-file environment/s3-bucket/s3-bucket.yml   \
+          --parameter-overrides     \
+            BucketName=$(S3_BUCKET_NAME)
+delete-s3-bucket:
+	./stack-deletion/delete-stack-wait-termination.sh $(S3_BUCKET_STACK_NAME)
